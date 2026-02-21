f65d0b2f3ff3e5c1060ca7c4ee2238e264300242
Uchenna <uezeilo@hotmail.com>
2026-02-21 23:17:10 +0000

    Update branding to vantedge

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,6 +13,22 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <GoogleAnalytics gaId="G-Q9X3P2G93S" />
+    </html>
+  )
+}
+
 
 export const metadata: Metadata = {
   title: {
