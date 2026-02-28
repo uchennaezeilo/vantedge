433edcdc251672744cd2a48828c15122a59ab289
Uchenna <uezeilo@hotmail.com>
2026-02-28 20:13:26 +0000

    Added more robust cookie consent manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,20 @@ export default function RootLayout({
         backendURL: '/api/c15t',
         consentCategories: ['necessary', 'marketing'], // Optional: Specify which consent categories to show in the banner.
         ignoreGeoLocation: true, // Useful for development to always view the banner.
+        legalLinks: {
+          privacyPolicy: {
+          href: '/privacy',
+          label: 'Privacy Policy',
+          },
+          cookiePolicy: {
+          href: '/cookies',
+          label: 'Cookie Policy',
+          },
+          termsOfService: {
+          href: '/terms',
+          label: 'Terms of Service',
+        },
+      },
       }}>
         <CookieBanner />
         <ConsentManagerDialog />
