b2ae6feef9112ddc809c98595a1baf25dbfbbe70
Uchenna <uezeilo@hotmail.com>
2026-03-08 00:49:50 +0000

    Added a head.tsx to enable google adsense in head tag

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4807336491663866"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
